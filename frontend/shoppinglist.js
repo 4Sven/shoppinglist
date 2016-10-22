2253f94d2b20494cd4a1d1eb53cc1b3202ade535
Sven Jerat <sven@jerat.de>
2016-10-22 13:26:39 +0200

    komma?

--- frontend/shoppinglist.js
+++ frontend/shoppinglist.js
@@ -198,7 +198,7 @@ angular.module('shoppingListApp', ['ui.bootstrap','ngResource','ngRoute','ngTouc
 						{
 							message : 'makeNewEntry:cancel',
 							then : 'GoToCart'
-						}
+						},
 						{
 							message : 'makeNewEntry:added',
 							then : 'makeNewEntry:addToCart'
